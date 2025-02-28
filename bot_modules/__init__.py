972103f35e2cacb5387e8d817a8492f3a2eb0d6e
Maissen369 <shalomc999@gmail.com>
2025-02-28 22:50:07 +0200

    Create __init__.py

---
+++ bot_modules/__init__.py
@@ -0,0 +1,13 @@
+# bot_modules/__init__.py
+
+from .files_operations import FilesOperations
+from .processes_operations import ProcessesOperations
+from .sys_operations import SysOperations
+from .network_operations import NetworkOperations
+
+__all__ = [
+    "FilesOperations",
+    "ProcessesOperations",
+    "SysOperations",
+    "NetworkOperations"
+]
